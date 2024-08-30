0512911f6dae154c324116e951e79f14ab9bfdb1
Shr4528 <shr4528@gmail.com>
2024-08-30 10:49:22 -0500

    redux

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,7 +1,33 @@
+import { Menu } from "lucide-react";
 
 
 const Sidebar = () => {
-  return <div>index</div>;
+  return(
+    <div>
+      {/* top logo */}
+    <div  className="flex gap-4 justify-between md:justify-normal items-center pt-8"  >
+      <div className="">logo</div>
+      <h1 className="font-extrabold  text-2xl">Stock</h1>
+    
+    <button className="md:hidden px-3 py-3 bg-gray-300 rounded-full hover:bg-blue-200" 
+    onClick={() => {}}>
+      <Menu className="w-4 h-4" />
+       
+    </button>
+    </div>
+{/* links */}
+    <div className="flex-grow mt-8">
+      {/* links here */}
+    </div>
+
+
+    {/* footer */}
+    <div>
+      <p className="text-center text-xs text-gray-500">&copy; 2024 Shamil</p>
+    </div>
+    </div>
+   
+  )
 };
 
 export default Sidebar;
