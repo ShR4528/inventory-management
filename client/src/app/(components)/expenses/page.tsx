2340024531ea9a370f53557fd578dbfd4a754af6
Shr4528 <shr4528@gmail.com>
2024-09-16 16:32:47 -0500

    expenses

--- client/src/app/(components)/expenses/page.tsx
+++ client/src/app/(components)/expenses/page.tsx
@@ -2,6 +2,7 @@
 
 import { useGetExpensesByCategoryQuery } from '@/app/state/api'
 import React, { useMemo, useState } from 'react'
+import Header from '../Header'
 
 
 const Expenses = () => {
@@ -21,7 +22,48 @@ const Expenses = () => {
     if(isError || !expensesData) return <div className="text-center text-red-500">Failed to fetch expenses</div>
 
   return (
-    <div>Expenses</div>
+    <div>
+
+   
+    {/*  HEADER */}
+    <div className='mb-5'>
+        <Header name='Expenses'  />
+        <p className='text-sm text-gray-500'>
+        A visual representation of your expenses.
+        </p>
+    </div>
+
+{/* FILTERS */}
+    <div className='flex flex-col md:flex-row justify-between gap-4'>
+        <div className='w-full md:w-1/3 bg-white shadow rounded-lg p-6'>
+        <h3 className='text-lg font-semibold mb-4'>
+            Filter by Category and Date
+        </h3>
+
+        <div className='space-y-4'>
+            {/* CATEGORY */}
+            <div>
+                <label htmlFor="category" className='block text-sm font-medium text-gray-700'>
+                    Category
+                </label>
+                <select
+                 name="category" 
+                id="category"
+                className='mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md'
+                onChange={(e) => setSelectedCategory(e.target.value)}
+                >
+                    <option>All</option>
+                    <option>Office</option>
+                    <option></option>
+                    <option></option>
+                </select>
+            </div>
+
+        </div>
+
+        </div>
+    </div>
+</div>
   )
 }
 
