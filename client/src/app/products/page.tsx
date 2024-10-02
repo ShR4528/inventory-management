1408d70bed499e826d48a37c3f0be2bd7eb22996
Shr4528 <shr4528@gmail.com>
2024-10-02 14:00:57 -0500

    fix

--- client/src/app/products/page.tsx
+++ client/src/app/products/page.tsx
@@ -44,6 +44,8 @@ const Products = () => {
     );
   }
 
+  const randomImageNumber = Math.floor(Math.random() * 3) + 1;
+
   return (
     <div className='mx-auto  pb-5 w-full'>
       {/* search bar */}
@@ -72,8 +74,7 @@ const Products = () => {
             <div key={product.productId} className='border shadow rounded-md p-4 max-w-full w-full mx-auto'>
               <div className='flex flex-col items-center'>
                 <Image
-                  src={`https://s3inventorymanagement.s3.us-east-2.amazonaws.com/product${Math.floor(Math.random() * 3) + 1
-                    }.png`}
+                   src={`https://s3nventorymanagement.s3.us-east-2.amazonaws.com/product${randomImageNumber}.png`}
                   alt={product.name}
                   width={150}
                   height={150}
